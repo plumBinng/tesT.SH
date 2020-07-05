2b72aa0305227997c327d6f61422946733cebbef
plumBinng <91535981+plumBinng@users.noreply.github.com>
2020-07-05 17:17:55 +0000

    type hinting

--- CryptoWalletValidator/AppDelegate.swift
+++ CryptoWalletValidator/AppDelegate.swift
@@ -14,4 +14,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Over
+        // Override point for custom
