72b1071e6690b97dad2eaa7be048615ed33091db
plumBinng <91535981+plumBinng@users.noreply.github.com>
2020-07-05 21:37:50 +0000

    add travis settings

--- CryptoWalletValidator/AppDelegate.swift
+++ CryptoWalletValidator/AppDelegate.swift
@@ -14,4 +14,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for custom
+        // Override point for customization after application laun
