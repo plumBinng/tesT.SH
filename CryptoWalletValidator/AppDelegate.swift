23a37cbd43ab1f2494b88a2770b0dfe71f915039
plumBinng <91535981+plumBinng@users.noreply.github.com>
2020-07-05 09:25:01 +0000

    fixed scope

--- CryptoWalletValidator/AppDelegate.swift
+++ CryptoWalletValidator/AppDelegate.swift
@@ -13,4 +13,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApp
+    func application(_ application: UIApplication, didFinishLaunchingWithOpt
