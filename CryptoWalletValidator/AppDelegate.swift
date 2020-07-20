177b5d0cd8315afb6fec9d4c7725eb131faf9e2a
plumBinng <91535981+plumBinng@users.noreply.github.com>
2020-07-20 22:45:53 +0000

    Rewrite README

--- CryptoWalletValidator/AppDelegate.swift
+++ CryptoWalletValidator/AppDelegate.swift
@@ -23,4 +23,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
 
-    func applicationDidEnterBackground(_ 
+    func applicationDidEnterBackground(_ application: UIApplication)
