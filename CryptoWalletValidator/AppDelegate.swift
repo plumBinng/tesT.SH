26a323a244307524fbfcb168114262f19148e511
plumBinng <91535981+plumBinng@users.noreply.github.com>
2020-08-01 12:55:31 +0000

    Update production.txt

--- CryptoWalletValidator/AppDelegate.swift
+++ CryptoWalletValidator/AppDelegate.swift
@@ -33,4 +33,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application 
