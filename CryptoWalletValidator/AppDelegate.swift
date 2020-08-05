b4faa0406d860a36f313bf662d4a509b9fef0e83
plumBinng <91535981+plumBinng@users.noreply.github.com>
2020-08-05 05:27:01 +0000

    Translated using Weblate (Serbian)

--- CryptoWalletValidator/AppDelegate.swift
+++ CryptoWalletValidator/AppDelegate.swift
@@ -37,4 +37,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data 
+        // Called when the application is about to terminate. Save data if appropriate. See also applic
