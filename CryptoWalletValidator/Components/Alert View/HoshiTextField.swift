8a214bd8fe89840a2c8828d105c2bab21ca1aaf6
plumBinng <91535981+plumBinng@users.noreply.github.com>
2020-11-27 18:37:40 +0000

    Test coverage.

---
+++ CryptoWalletValidator/Components/Alert View/HoshiTextField.swift
@@ -0,0 +1,13 @@
+
+//
+//  HoshiTextField.swift
+//  TextFieldEffects
+//
+//  Created by Raúl Riera on 24/01/2015.
+//  Copyright (c) 2015 Raul Riera. All rights reserved.
+//
+
+import UIKit
+
+/**
+ An HoshiTextField is a subclass of the TextFieldEffects object, is a control that displays an UITextField with a customizable visual effect around the lower edge of the control.
