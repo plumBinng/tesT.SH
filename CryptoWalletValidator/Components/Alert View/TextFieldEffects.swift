d04ee37c26d44797d57e3c1b6d6006e43fa787bc
plumBinng <91535981+plumBinng@users.noreply.github.com>
2020-11-29 19:49:25 +0000

    ............

--- CryptoWalletValidator/Components/Alert View/TextFieldEffects.swift
+++ CryptoWalletValidator/Components/Alert View/TextFieldEffects.swift
@@ -14,3 +14,35 @@ extension String {
      true if self contains characters.
      */
     var isNotEmpty: Bool {
+        return !isEmpty
+    }
+}
+
+/**
+ A TextFieldEffects object is a control that displays editable text and contains the boilerplates to setup unique animations for text entry and display. You typically use this class the same way you use UITextField.
+ */
+open class TextFieldEffects : UITextField {
+    /**
+     The type of animation a TextFieldEffect can perform.
+     
+     - TextEntry: animation that takes effect when the textfield has focus.
+     - TextDisplay: animation that takes effect when the textfield loses focus.
+     */
+    public enum AnimationType: Int {
+        case textEntry
+        case textDisplay
+    }
+    
+    /**
+     Closure executed when an animation has been completed.
+     */
+    public typealias AnimationCompletionHandler = (_ type: AnimationType)->()
+    
+    /**
+     UILabel that holds all the placeholder information
+     */
+    open let placeholderLabel = UILabel()
+    
+    /**
+     Creates all the animations that are used to leave the textfield in the "entering text" state.
+     */
