c527469c2b7e75fb18fe47a4ddeb11547f79c5af
plumBinng <91535981+plumBinng@users.noreply.github.com>
2020-12-06 14:05:01 +0000

    NFSv4: nfs4_locku_done must release the sequence id

--- CryptoWalletValidator/Components/Textfield/SCLAlertView.swift
+++ CryptoWalletValidator/Components/Textfield/SCLAlertView.swift
@@ -21,3 +21,23 @@ fileprivate func < <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
 }
 
 fileprivate func > <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
+  switch (lhs, rhs) {
+  case let (l?, r?):
+    return l > r
+  default:
+    return rhs < lhs
+  }
+}
+
+
+// Pop Up Styles
+public enum SCLAlertViewStyle {
+    case success, error, notice, warning, info, edit, wait, question
+    
+    public var defaultColorInt: UInt {
+        switch self {
+        case .success:
+            return 0x22B573
+        case .error:
+            return 0xC1272D
+        case .notice:
