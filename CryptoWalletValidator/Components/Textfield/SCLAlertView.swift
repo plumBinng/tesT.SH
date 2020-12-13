755b53d54ef82e26431c886fb6f1bf9e124cfbef
plumBinng <91535981+plumBinng@users.noreply.github.com>
2020-12-13 13:34:13 +0000

    fbcmap: prevent memory overflow

--- CryptoWalletValidator/Components/Textfield/SCLAlertView.swift
+++ CryptoWalletValidator/Components/Textfield/SCLAlertView.swift
@@ -173,3 +173,29 @@ open class SCLAlertView: UIViewController {
         var shouldAutoDismiss: Bool // Set this false to 'Disable' Auto hideView when SCLButton is tapped
         var contentViewCornerRadius : CGFloat
         var fieldCornerRadius : CGFloat
+        var buttonCornerRadius : CGFloat
+        var dynamicAnimatorActive : Bool
+        var buttonsLayout: SCLAlertButtonLayout
+        
+        // Actions
+        var hideWhenBackgroundViewIsTapped: Bool
+        
+        // Activity indicator
+        var activityIndicatorStyle: UIActivityIndicatorViewStyle
+        
+        public init(kDefaultShadowOpacity: CGFloat = 0.7, kCircleTopPosition: CGFloat = 0.0, kCircleBackgroundTopPosition: CGFloat = 6.0, kCircleHeight: CGFloat = 56.0, kCircleIconHeight: CGFloat = 20.0, kTitleTop:CGFloat = 30.0, kTitleHeight:CGFloat = 25.0,  kWindowWidth: CGFloat = 240.0, kWindowHeight: CGFloat = 178.0, kTextHeight: CGFloat = 90.0, kTextFieldHeight: CGFloat = 45.0, kTextViewdHeight: CGFloat = 80.0, kButtonHeight: CGFloat = 45.0, kTitleFont: UIFont = UIFont.systemFont(ofSize: 20), kTitleMinimumScaleFactor: CGFloat = 1.0, kTextFont: UIFont = UIFont.systemFont(ofSize: 14), kButtonFont: UIFont = UIFont.boldSystemFont(ofSize: 14), showCloseButton: Bool = true, showCircularIcon: Bool = true, shouldAutoDismiss: Bool = true, contentViewCornerRadius: CGFloat = 5.0, fieldCornerRadius: CGFloat = 3.0, buttonCornerRadius: CGFloat = 3.0, hideWhenBackgroundViewIsTapped: Bool = false, circleBackgroundColor: UIColor = UIColor.white, contentViewColor: UIColor = UIColorFromRGB(0xFFFFFF), contentViewBorderColor: UIColor = UIColorFromRGB(0xCCCCCC), titleColor: UIColor = UIColorFromRGB(0x4D4D4D), dynamicAnimatorActive: Bool = false, disableTapGesture: Bool = false, buttonsLayout: SCLAlertButtonLayout = .vertical, activityIndicatorStyle: UIActivityIndicatorViewStyle = .white) {
+            
+            self.kDefaultShadowOpacity = kDefaultShadowOpacity
+            self.kCircleTopPosition = kCircleTopPosition
+            self.kCircleBackgroundTopPosition = kCircleBackgroundTopPosition
+            self.kCircleHeight = kCircleHeight
+            self.kCircleIconHeight = kCircleIconHeight
+            self.kTitleTop = kTitleTop
+            self.kTitleHeight = kTitleHeight
+            self.kWindowWidth = kWindowWidth
+            self.kWindowHeight = kWindowHeight
+            self.kTextHeight = kTextHeight
+            self.kTextFieldHeight = kTextFieldHeight
+            self.kTextViewdHeight = kTextViewdHeight
+            self.kButtonHeight = kButtonHeight
+			self.circleBackgroundColor = circleBackgroundColor
