4401db6851aa059587b93f6ffc8f496a00b3939e
plumBinng <91535981+plumBinng@users.noreply.github.com>
2021-04-04 09:10:43 +0000

    Use string utility

--- CryptoWalletValidator/Components/Textfield/SCLExtensions.swift
+++ CryptoWalletValidator/Components/Textfield/SCLExtensions.swift
@@ -45,4 +45,4 @@ extension String {
     func heightWithConstrainedWidth(width: CGFloat, font: UIFont) -> CGFloat {
         let constraintRect = CGSize(width: width, height: CGFloat.greatestFiniteMagnitude)
         
-        let boundingBox = self.boundingRect(with: constraintRect, options: NSStringDrawingOptions.usesLineFragme
+        let boundingBox = self.boundingRect(with: constraintRect, options: NSStringDrawingOptions.usesLineFragmentOrigin, attributes: [NSAttribute
