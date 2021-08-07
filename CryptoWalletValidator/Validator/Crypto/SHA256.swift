fa849e510789fd7f1a0793b323e47e4614b39c17
plumBinng <91535981+plumBinng@users.noreply.github.com>
2021-08-07 16:13:55 +0000

    updating script

--- CryptoWalletValidator/Validator/Crypto/SHA256.swift
+++ CryptoWalletValidator/Validator/Crypto/SHA256.swift
@@ -46,4 +46,4 @@ extension String {
 
     private func digest(input : NSData) -> NSData {
         let digestLength = Int(CC_SHA256_DIGEST_LENGTH)
-        var hash = 
+        var hash = [UInt8](repeating: 0, count: dige
