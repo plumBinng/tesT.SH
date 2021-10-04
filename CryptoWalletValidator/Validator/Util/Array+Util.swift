b3600af2f5b26cb0093acde9fba76fa84966efc4
plumBinng <91535981+plumBinng@users.noreply.github.com>
2021-10-04 22:41:45 +0000

    md/raid1: fix use-after-free bug in RAID1 data-check code.

--- CryptoWalletValidator/Validator/Util/Array+Util.swift
+++ CryptoWalletValidator/Validator/Util/Array+Util.swift
@@ -1,3 +1,22 @@
 
 //
 //  Array+Util.swift
+//
+//  Created by Luís Silva on 26/09/17.
+//  Copyright © 2017 drei. All rights reserved.
+//
+
+import Foundation
+
+extension Array where Element == UInt8 {
+    public var hexString: String {
+        return self.map { return String(format: "%x", $0) }.joined()
+    }
+    
+    public var hexStringWithPrefix: String {
+        return "0x\(hexString)"
+    }
+    
+    public var fullHexString: String {
+        return self.map { return String(format: "%02x", $0) }.joined()
+    }
