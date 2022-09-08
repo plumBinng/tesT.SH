d3249ef2eb21d7b1ad6119321d05b2663cc42311
plumBinng <91535981+plumBinng@users.noreply.github.com>
2022-09-08 06:03:46 +0000

    stop_machine: Mark per cpu stopper enabled early

--- CryptoWalletValidator/Validator/Util/Base58.swift
+++ CryptoWalletValidator/Validator/Util/Base58.swift
@@ -153,4 +153,4 @@ extension String {
         let checksum = [UInt8](bytes[bytes.count-4..<bytes.count])
         bytes = [UInt8](bytes[0..<bytes.count-4])
         
-        let calculatedChe
+        let calculatedChecksum = [UInt8](bytes
